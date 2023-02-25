e18ad62dfee5173a0236623ade706d2d1e25d049
vanilnic <bikadog306@gmail.com>
2023-02-25 14:36:35 +0300

    bogdantop

---
+++ ClientServerSocket/Client/src/Client.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import bogdan.Phone;
+
+public class Client {
+    public static void main(String[] args) {
+        try(Phone phone = new Phone("127.0.0.1", 8000)) {
+            System.out.println("Connected to server");
+
+            String request = "Visaginas";
+            System.out.println("Request: " + request);
+            phone.writeLine(request);
+
+            String response = phone.readLine();
+            System.out.println("Response: " + response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ClientServerSocket/Server/src/Server.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.ServerSocket;
+import bogdan.Phone;
+
+public class Server {
+    public static void main(String[] args) {
+
+        try (ServerSocket server = new ServerSocket(8000)) {
+            System.out.println("Server started!");
+
+            while (true) {
+                Phone phone = new Phone(server);
+                new Thread(() -> {
+                    String request = phone.readLine();
+                    System.out.println("Request: " + request);
+                    String response = (int) (Math.random() * 30 - 10) + "";
+                    try {Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    phone.writeLine(response);
+                    System.out.println("Response: " + response);
+                    try { phone.close(); } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).start();
+            }
+
+            } catch(IOException e){
+                throw new RuntimeException(e);
+            }
+    }
+}
+
